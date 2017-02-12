3fd18dd82277566b351380b609c4693ee7cda652
evgeniyshchukin <evgeniy.shchukin@gmail.com>
2017-02-12 13:10:20 +0300

    Done module6-task1

--- js/form.js
+++ js/form.js
@@ -1,71 +1,42 @@
 'use strict';
 
-// Отрисовка меток на карте
-var dialog = document.querySelector('.dialog');
-var dialogClose = document.querySelector('.dialog__close');
-var pinMap = document.querySelector('.tokyo__pin-map');
-var pins = document.querySelectorAll('.pin');
+window.initializeForm = (function () {
+  var noticeTimeIn = document.querySelector('#time');
+  var noticeTimeOut = document.querySelector('#timeout');
+  var noticeTimesIn = ['12', '13', '14'];
+  var noticeTimesOut = ['12', '13', '14'];
 
-window.initializePins(pinMap, pins, 'pin', 'pin--active', dialog, dialogClose);
+  var noticeRoomNumber = document.querySelector('#room_number');
+  var noticeCapacity = document.querySelector('#capacity');
+  var noticeRoomNumbers = ['1', '2', '100'];
+  var noticeCapacities = ['0', '3', '3'];
 
-// Синхнонизация времени заезда и выезда
-var noticeTimeIn = document.querySelector('#time');
-var noticeTimeOut = document.querySelector('#timeout');
-var noticeTimesIn = [
-  '12',
-  '13',
-  '14'
-];
-var noticeTimesOut = [
-  '12',
-  '13',
-  '14'
-];
+  var noticeType = document.querySelector('#type');
+  var noticePrice = document.querySelector('#price');
+  var noticeTypes = ['flat', 'shack', 'palace'];
+  var noticePrices = ['1000', '500', '10000'];
 
-window.synchronizeFields(noticeTimeIn, noticeTimeOut, noticeTimesIn, noticeTimesOut, 'value');
+  var noticeTitle = document.querySelector('#title');
+  var noticeAddress = document.querySelector('#address');
 
-// Синхнонизация количества комнат и количества мест
-var noticeRoomNumber = document.querySelector('#room_number');
-var noticeCapacity = document.querySelector('#capacity');
-var noticeRoomNumbers = [
-  '1',
-  '2',
-  '100'
-];
-var noticeCapacities = [
-  '0',
-  '3',
-  '3'
-];
+  // Синхнонизация времени заезда и выезда
+  window.synchronizeFields(noticeTimeIn, noticeTimeOut, noticeTimesIn, noticeTimesOut, 'value');
 
-window.synchronizeFields(noticeRoomNumber, noticeCapacity, noticeRoomNumbers, noticeCapacities, 'value');
+  // Синхнонизация количества комнат и количества мест
+  window.synchronizeFields(noticeRoomNumber, noticeCapacity, noticeRoomNumbers, noticeCapacities, 'value');
 
-// Синхронизация типа жилья и цены
-var noticeType = document.querySelector('#type');
-var noticePrice = document.querySelector('#price');
-var noticeTypes = [
-  'flat',
-  'shack',
-  'palace'
-];
-var noticePrices = [
-  '1000',
-  '0',
-  '10000'
-];
+  // Синхронизация типа жилья и цены
+  window.synchronizeFields(noticeType, noticePrice, noticeTypes, noticePrices, 'value');
 
-window.synchronizeFields(noticeType, noticePrice, noticeTypes, noticePrices, 'value');
+  // Изменение параметров у элементов в HTML
+  noticeTitle.required = true;
+  noticeTitle.minLength = 30;
+  noticeTitle.maxLength = 100;
 
-// Изменение параметров у элементов в HTML
-var noticeTitle = document.querySelector('#title');
-var noticeAddress = document.querySelector('#address');
-noticeTitle.required = true;
-noticeTitle.minLength = 30;
-noticeTitle.maxLength = 100;
+  noticePrice.type = 'number';
+  noticePrice.min = 1000;
+  noticePrice.max = 1000000;
+  noticePrice.placeholder = 'от 1000';
 
-noticePrice.type = 'number';
-noticePrice.min = 1000;
-noticePrice.max = 1000000;
-noticePrice.placeholder = 'от 1000';
-
-noticeAddress.required = true;
+  noticeAddress.required = true;
+})();
--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,72 +1,16 @@
-'use strict';
+'use strict'
 
-var ENTER_KEY_CODE = 13;
+window.initializePins = (function () {
+  var parentElement = document.querySelector('.tokyo__pin-map');
 
-window.initializePins = function (parentElement, elementsArray, className, classNameActive, dialogName, dialogNameClose) {
   parentElement.addEventListener('click', function (event) {
-    activeHandler(event);
+    window.utils.activateElement(event, parentElement);
   });
 
   parentElement.addEventListener('keydown', function (event) {
-    if (isActivateElement(event)) {
-      activeHandler(event);
+    if (window.utils.isActivationEvent(event)) {
+      window.utils.activateElement(event, parentElement);
     }
   });
 
-  var activeHandler = function (event) {
-    var activeElement = event.target;
-
-    while (activeElement !== parentElement) {
-      if (activeElement.classList.contains(className)) {
-        if (activeElement.classList.contains(classNameActive)) {
-          activeElement.classList.remove(classNameActive);
-          dialogClose();
-          activeElement.setAttribute('aria-pressed', false);
-        } else {
-          activeElementsDelete();
-          dialogOpen();
-          activeElement.classList.add(classNameActive);
-          activeElement.setAttribute('aria-pressed', true);
-        }
-        return;
-      }
-      activeElement = activeElement.parentNode;
-    }
-  };
-
-  var isActivateElement = function (event) {
-    return event.keyCode && event.keyCode === ENTER_KEY_CODE;
-  };
-
-  var dialogClose = function () {
-    dialogName.style.display = 'none';
-    dialogNameClose.removeEventListener('click', clickHandler);
-    dialogNameClose.removeEventListener('click', keyHandler);
-  };
-
-  var dialogOpen = function () {
-    dialogName.style.display = 'block';
-    dialogNameClose.addEventListener('click', clickHandler);
-    dialogNameClose.addEventListener('click', keyHandler);
-  };
-
-  var clickHandler = function () {
-    dialogClose();
-    activeElementsDelete();
-  };
-
-  var keyHandler = function (event) {
-    if (isActivateElement(event)) {
-      dialogClose();
-      activeElementsDelete();
-    }
-  };
-
-  var activeElementsDelete = function () {
-    for (var i = 0; i < elementsArray.length; i++) {
-      elementsArray[i].classList.remove(classNameActive);
-      elementsArray[i].setAttribute('aria-pressed', false);
-    }
-  };
-
-};
+})();
