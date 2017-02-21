7c62ef730edbb4994c1bf058a4c8f01785665123
evgeniyshchukin <evgeniy.shchukin@gmail.com>
2017-02-21 18:49:57 +0300

    Done Module7 task1

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,16 +1,104 @@
 'use strict';
 
+// МОДУЛЬ ОТРИСОВКИ ПИНОВ И НАВЕШИВАНИЯ НА НИХ ОЖИДАНИЯ ПО КЛИКУ И КЛАВИШАМ
 window.initializePins = (function () {
-  var parentElement = document.querySelector('.tokyo__pin-map');
-
-  parentElement.addEventListener('click', function (event) {
-    window.utils.activateElement(event, parentElement);
+  // ЗАГРУЗКА ДАННЫХ С СЕРВЕРА
+  window.load('https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data', function (data) {
+    // Записываем в массив данные, полученные из JSON
+    window.similarApartments = data;
+    // Вызываем функцию для отрисовки нового пина
+    renderPins(similarApartments.length, parentElement);
   });
 
+  // ФУНКЦИЯ ОТРИСОВКИ ПИНОВ ИЗ ШАБЛОНА
+  var renderPins = function (amount, array) {
+    // Находим шаблон
+    var templateElement = document.querySelector('#pin-template');
+    // То, что будем копировать в шаблоне
+    var elementToClone = templateElement.content.querySelector('.pin');
+
+    // Создаем цикл, чтобы пройтись по пинам
+    for (var i = 0; i < amount; i++) {
+      // клонируем новый пин из шаблона
+      var newPin = elementToClone.cloneNode(true);
+      var imageOfPin = newPin.querySelector('.rounded');
+
+      // Изменение аватарки пина
+      imageOfPin.src = window.similarApartments[i].author.avatar;
+
+      // Значения координат из data
+      newPin.style.left = window.similarApartments[i].location.x + 'px';
+      newPin.style.top = window.similarApartments[i].location.y + 'px';
+
+      // Вставляем пин в DOM
+      array.appendChild(newPin);
+    }
+  };
+
+  // НАВЕШИВАНИЕ ОЖИДАНИЯ НА ПИНЫ
+  // Определяем область в к которой будем находить пины
+  var parentElement = document.querySelector('.tokyo__pin-map');
+  // Определяем теги для поиска нужных пинов и настроек
+  var className = 'pin';
+  var classNameActive = 'pin--active';
+
+  var ENTER_KEY_CODE = 13;
+
+  // Определяем функцию активирования пинов
+  function activateElement(event, parentElement) {
+    window.activeElement = event.target;
+    var activeElement = window.activeElement;
+
+    while (activeElement !== parentElement) {
+      if (activeElement.classList.contains(className)) {
+        if (activeElement.classList.contains(classNameActive)) {
+          activeElement.classList.remove(classNameActive);
+          document.querySelector('.dialog').remove();
+          activeElement.setAttribute('aria-pressed', false);
+        } else {
+          deactivateElements();
+          activeElement.classList.add(classNameActive);
+          activeElement.setAttribute('aria-pressed', true);
+          window.showCard();
+        }
+        return;
+      }
+      activeElement = activeElement.parentNode;
+    }
+  }
+
+  // Определяем функцию деактивирования пинов
+  var deactivateElements = function () {
+    var elementsArray = document.querySelectorAll('.pin');
+
+    for (var i = 0; i < elementsArray.length; i++) {
+      elementsArray[i].classList.remove(classNameActive);
+      elementsArray[i].setAttribute('aria-pressed', false);
+    }
+  }
+
+  // Определяем функцию проверки нажатия клавишы
+  var isActivationEvent = function (event) {
+    return event.keyCode && event.keyCode === ENTER_KEY_CODE;
+  };
+
+  // Навешиваем ожидание по клику на пины;
+  parentElement.addEventListener('click', function (event) {
+    activateElement(event, parentElement);
+  });
+
+  // Навешиваем ожидание по нажатию на клавишу на пины;
   parentElement.addEventListener('keydown', function (event) {
-    if (window.utils.isActivationEvent(event)) {
-      window.utils.activateElement(event, parentElement);
+    if (isActivationEvent(event)) {
+      activateElement(event, parentElement);
+      document.querySelector('.dialog__close').focus();
     }
   });
 
+  // Возвращаем в глобальную область функцию проверки нажатия клавиши
+  return {
+    isActivationEvent: isActivationEvent,
+
+    deactivateElements: deactivateElements
+  };
 })();
--- js/show-card.js
+++ js/show-card.js
@@ -1,6 +1,112 @@
 'use strict';
 
+// ФУНКЦИЯ ОТРИСОВКИ ДИАЛОГОВОГО ОКНА
 window.showCard = function () {
+  var parentDialog = document.querySelector('.tokyo');  // определяем блок, куда будем вставлять шаблон
+  var templateDialog = document.querySelector('#dialog-template'); // находим шаблон
+  var dialogToClone = templateDialog.content.querySelector('.dialog'); // то что будем копировать в шаблоне
+
+  // Описываем функцию отрисовки диалога
+  function renderDialog() {
+    // Проверяем есть ли открытые диалоги и если они есть - удаляем
+    if (document.querySelector('.dialog')) {
+      document.querySelector('.dialog').remove();
+    }
+
+    // Определяем номер активного пина, для того, чтобы соотнести с нужным диалогом
+    var elementsArray = document.querySelectorAll('.pin');
+    var classNameActive = 'pin--active';
+
+    for (var i = 0; i < elementsArray.length; i++) {
+      if(elementsArray[i].classList.contains(classNameActive)) {
+        var elementIndex = i;
+      }
+    }
+
+    // Копируем диалог из шаблона
+    var newDialog = dialogToClone.cloneNode(true);
+
+    // Наполняем диалог содержимым
+    var dialogAvatar = newDialog.querySelector('.dialog__avatar');
+    var dialogTitle = newDialog.querySelector('.lodge__title');
+    var dialogAddress = newDialog.querySelector('.lodge__address');
+    var dialogPrice = newDialog.querySelector('.lodge__price');
+    var dialogType = newDialog.querySelector('.lodge__type');
+    var dialogRoomsAndGuests = newDialog.querySelector('.lodge__rooms-and-guests');
+    var dialogCheckinTime = newDialog.querySelector('.lodge__checkin-time');
+    var dialogFeatures = newDialog.querySelector('.lodge__features');
+    var dialogDescription = newDialog.querySelector('.lodge__description');
+    var dialogPhotos = newDialog.querySelector('.lodge__photos');
+
+
+    dialogAvatar.src = window.similarApartments[elementIndex - 1].author.avatar;
+    dialogTitle.innerText = window.similarApartments[elementIndex - 1].offer.title;
+    dialogAddress.innerText = window.similarApartments[elementIndex - 1].offer.address;
+    dialogPrice.innerText = window.similarApartments[elementIndex - 1].offer.price;
+    dialogType.innerText = window.similarApartments[elementIndex - 1].offer.type;
+    dialogRoomsAndGuests.innerText = 'Комнат: ' + window.similarApartments[elementIndex - 1].offer.rooms + ', Мест: ' +  window.similarApartments[elementIndex - 1].offer.guests;
+    dialogCheckinTime.innerHTML = 'Время заезда: ' + window.similarApartments[elementIndex - 1].offer.checkin + '<br>' + 'Время выезда: ' +  window.similarApartments[elementIndex - 1].offer.checkout;
+
+    for (i = 0; i < window.similarApartments[elementIndex - 1].offer.features.length; i++) {
+      var featureImage = document.createElement('span');
+
+      featureImage.classList.add('feature__image');
+      featureImage.classList.add('feature__image--' + window.similarApartments[elementIndex - 1].offer.features[i]);
+
+      dialogFeatures.appendChild(featureImage);
+    }
+
+    dialogDescription.innerText = window.similarApartments[elementIndex - 1].offer.description;
+
+    for (i = 0; i < window.similarApartments[elementIndex - 1].offer.photos.length; i++) {
+      var dialogPhoto = document.createElement('img');
+
+      dialogPhoto.setAttribute('width', '52');
+      dialogPhoto.setAttribute('height', '42');
+      dialogPhoto.setAttribute('alt', 'Фотография места');
+      dialogPhoto.setAttribute('src', window.similarApartments[elementIndex - 1].offer.photos[i]);
+
+      dialogPhotos.appendChild(dialogPhoto);
+    }
+
+    // Вставляем получившийся диалог в указанный блок
+    parentDialog.appendChild(newDialog);
+  }
+
+  // Запускаем отрисовку диалога
+  renderDialog();
+
+  // Определяем элементы для работы с диалогом
   var dialogName = document.querySelector('.dialog');
-  dialogName.style.display = 'block';
+  var dialogNameClose = document.querySelector('.dialog__close');
+
+  var ENTER_KEY_CODE = 13;
+
+  // Определяем обработчики ожидания
+  var clickHandler = function () {
+    dialogClose();
+    window.initializePins.deactivateElements();
+  };
+
+  var keyHandler = function () {
+    if (isActivationEvent(event)) {
+      dialogClose();
+      window.initializePins.deactivateElements();
+    }
+  };
+
+  // Определяем функцию закрытия диалога
+  var dialogClose = function () {
+    dialogNameClose.removeEventListener('click', clickHandler);
+    dialogNameClose.removeEventListener('keydown', keyHandler);
+
+    window.activeElement.focus();
+    document.querySelector('.dialog').remove();
+  }
+
+  // Навешиваем обработчик по клику
+  dialogNameClose.addEventListener('click', clickHandler);
+
+  // Показываем диалоговое окно
+  // dialogName.style.display = 'block';
 };
